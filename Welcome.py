8b4ee691cf9ebcab667258428a06c95de899b1e3
pollywestspring <141242166+pollywestspring@users.noreply.github.com>
2023-08-03 10:05:29 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -14,5 +14,6 @@ st.dataframe(df)
 
 el_list=df.columns.tolist()[27:80]
 x_axis=st.selectbox('select element', el_list)
-y_axis=st.selectbox('select element', el_list)
+
+st.multiselect('select location', file_name_list, file_name_list[0])
 
