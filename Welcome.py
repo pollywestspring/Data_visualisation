0151c900cdbab1c10d9af198308cb13bac753d30
pollywestspring <141242166+pollywestspring@users.noreply.github.com>
2023-08-03 09:44:19 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -12,5 +12,7 @@ st.write('Hello world')
 df=pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
-el_listdf.columns.tolist()[27:80]
+el_list=df.columns.tolist()[27:80]
 x_axis=st.selectbox('select element", el_list)
+
+st.selectbox('select location', file_name_list)
