3c851e13f85c9a21d313a0056489562ff3cb24b9
pollywestspring <141242166+pollywestspring@users.noreply.github.com>
2023-08-03 10:01:16 +0200

    Update Welcome.py

--- Welcome.py
+++ Welcome.py
@@ -7,12 +7,12 @@ for i in os.listdir(): #select every csv file in my directory
   if i.endswith('csv'):
                 file_name_list.append(i)
 
-st.write('Hello world')
+st.write('Hello world of visualisation')
 
 df=pd.read_csv('Bastar Craton.csv')
 st.dataframe(df)
 
 el_list=df.columns.tolist()[27:80]
 x_axis=st.selectbox('select element', el_list)
+y_axis=st.selectbox('select element', el_list)
 
-st.multiselect('select location', file_name_list,file_name_list[0])
